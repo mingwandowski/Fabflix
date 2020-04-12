e5f79da1c286843e624b2bee70366e4771e1e515
mingwandowski <haoming2019@gmail.com>
2020-04-11 23:28:09 -0400

    move provate team repository to my own

---
+++ README.md
@@ -0,0 +1,13 @@
+## project1
+
+### almost finished
+
+Now we have three servlet files: MoviesServlet, SingleMovieServlet, SingleStarServlet.
+
+Three HTML pages with paired JavaScript pages: index.html, single-movie.html, single-star.html
+
+
+### TODO
+1. Demo video
+2. README report
+3. CSS decoration
---
+++ src/SingleMovieServlet.java
@@ -0,0 +1,137 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
+@WebServlet(name = "SingleMovieServlet", urlPatterns = "/api/single-movie")
+public class SingleMovieServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Retrieve parameter id from url request.
+        String id = request.getParameter("id");
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Construct a query with parameter represented by "?"
+            String movieQuery = "select movies.*, ratings.rating from movies, ratings " +
+                           "where movies.id = ratings.movieId and movies.id = ? ;";
+
+            String genresQuery = "select genres.* from genres, genres_in_movies as gim " +
+                           "where genres.id = gim.genreId and gim.movieId = ? ;";
+
+            String starsQuery = "select stars.* from stars, stars_in_movies as sim " +
+                                "where stars.id = sim.starId and sim.movieId = ? ;";
+
+            // Declare our statement
+            PreparedStatement movieStatement = dbcon.prepareStatement(movieQuery);
+            PreparedStatement genresStatement = dbcon.prepareStatement(genresQuery);
+            PreparedStatement starsStatement = dbcon.prepareStatement(starsQuery);
+
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            movieStatement.setString(1, id);
+            genresStatement.setString(1, id);
+            starsStatement.setString(1, id);
+
+            // Perform the query
+            ResultSet movieRS = movieStatement.executeQuery();
+            ResultSet genresRS = genresStatement.executeQuery();
+            ResultSet starsRS = starsStatement.executeQuery();
+
+            // First, get movie info
+            movieRS.next();
+            String movieId = movieRS.getString("id");
+            String movieTitle = movieRS.getString("title");
+            String movieYear = movieRS.getString("year");
+            String movieDirector = movieRS.getString("director");
+            float rating = movieRS.getFloat("rating");
+
+            // Second, iterate and put genres info into genresJsonArray
+            JsonArray genresJsonArray = new JsonArray();
+            while(genresRS.next()){
+                String genreName = genresRS.getString("name");
+                genresJsonArray.add(genreName);
+            }
+
+            // Third, iterate and put stars info into starsJsonArray
+            JsonArray starsJsonArray = new JsonArray();
+            while(starsRS.next()){
+                String starId = starsRS.getString("id");
+                String starName = starsRS.getString("name");
+                String starDob = starsRS.getString("birthYear");
+
+                JsonObject starsJsonObject = new JsonObject();
+                starsJsonObject.addProperty("star_id", starId);
+                starsJsonObject.addProperty("star_name", starName);
+                starsJsonObject.addProperty("star_dob", starDob);
+
+                starsJsonArray.add(starsJsonObject);
+            }
+
+            // Put all properties into jsonObject
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("movie_id", movieId);
+            jsonObject.addProperty("movie_title", movieTitle);
+            jsonObject.addProperty("movie_year", movieYear);
+            jsonObject.addProperty("movie_director", movieDirector);
+            jsonObject.addProperty("movie_rating", rating);
+            jsonObject.add("genres", genresJsonArray);
+            jsonObject.add("stars", starsJsonArray);
+
+            // write JSON string to output
+            out.write(jsonObject.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            starsRS.close();
+            genresRS.close();
+            movieRS.close();
+            starsStatement.close();
+            genresStatement.close();
+            movieStatement.close();
+            dbcon.close();
+        } catch (Exception e) {
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // set reponse status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+        out.close();
+        //close it;
+
+    }
+
+}
